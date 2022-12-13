57c23a1b146bb348c9936d8234f8adf542b9c565
Brian Hill <brian@daily.co>
2022-12-12 21:54:44 -0500

    Day 12 2022

---
+++ 2022/day12/day12-part2.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+from functools import reduce
+import sys
+from typing import List, Tuple
+
+distance: List[List[int]]
+
+def sortNodes(i: Tuple[int, int]) -> int: return distance[i[0]][i[1]]
+
+def elevation(c: str) -> int:
+    if c == 'S':
+        return ord('a')
+    if c == 'E':
+        return ord('z')
+    return ord(c)
+
+def neighbors(i: Tuple[int, int], grid: List[List[str]], to_visit: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+    result = []
+    (y,x) = i
+    if y > 0 and elevation(grid[y-1][x]) > elevation(grid[y][x]) - 2 and (y-1,x) in to_visit:
+        result.append((y-1,x))
+    if y < len(grid) - 1 and elevation(grid[y+1][x]) > elevation(grid[y][x]) - 2 and (y+1,x) in to_visit:
+        result.append((y+1,x))
+    if x > 0 and elevation(grid[y][x-1]) > elevation(grid[y][x]) - 2 and (y,x-1) in to_visit:
+        result.append((y,x-1))
+    if x < len(grid[0]) - 1 and elevation(grid[y][x+1]) > elevation(grid[y][x]) - 2 and (y,x+1) in to_visit:
+        result.append((y,x+1))
+    return result
+
+
+with open("input.txt") as file:
+    lines = file.readlines()
+    grid = []
+    start = (0, 0)
+    for i in range(0, len(lines)):
+        l = lines[i].strip()
+        grid.append(list(l))
+
+        if l.find('E') != -1:
+            start = (i, l.find('E'))
+
+    to_visit = [ (y,x) for x in range(0, len(grid[0])) for y in range(0, len(grid)) ]
+    distance = [[ sys.maxsize for _ in range(0, len(grid[0]))] for _ in range(0, len(grid))]
+    previous: List[List[Tuple[int,int]]] = [[ (0, 0) for _ in range(0, len(grid[0]))] for _ in range(0, len(grid))]
+
+    distance[start[0]][start[1]] = 0
+    while len(to_visit) > 0:
+        to_visit.sort(key=sortNodes)
+        u = to_visit[0]
+        del to_visit[0]
+
+        for node in neighbors(u, grid, to_visit):
+            new_dist = distance[u[0]][u[1]] + 1
+            if new_dist < distance[node[0]][node[1]]:
+                distance[node[0]][node[1]] = new_dist
+                previous[node[0]][node[1]] = u
+    min_distance = sys.maxsize
+    for y in range(0, len(grid)):
+        for x in range(0, len(grid[0])):
+            if (grid[y][x] == 'a' or grid[y][x] == 'S') and distance[y][x] < min_distance:
+                min_distance = distance[y][x]
+
+
+    print(f"Minimum distance from E to any location with elevation a = {min_distance}")
+
